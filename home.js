2d1aea739aa0e3d5cad7cfc703a26ab26907a999
boypaida12 <baddysackey5@gmail.com>
2023-04-22 09:44:56 +0000

    fixed faq section js

--- home.js
+++ home.js
@@ -1,18 +1,6 @@
 let menuOpen = document.getElementById('menuOpen');
 let menuClose = document.getElementById('menuClose');
 let transparentBackground = document.getElementById('transparentBackground');
-// let faqQuestion = document.querySelectorAll('.faq-question');
-// let answer = document.querySelector('.faq-answer');
-// let displayNone = document.querySelector('#faqAnswer');
-const faqQuestion = document.querySelectorAll('.faq-question');
-const answer = document.querySelectorAll('.faq-answer');
-
-const plusSigns = document.querySelectorAll('.plus-sign');
-const minusSigns = document.querySelectorAll('.minus-sign');
-
-
-
-console.log(menuOpen);
 
 menuOpen.addEventListener ('click', function(){
     transparentBackground.style.display = "block";
@@ -22,57 +10,34 @@ menuClose.addEventListener ('click', function (){
     transparentBackground.style.display = "none";
 })
 
-// faqQuestion.addEventListener ('click', ()=>{
-//     console.log('question clicked')
-//     displayNone.style.display = displayNone.style.display === 'none'? 'block':'none';
-// })
+// FAQ fixed
+let questions = document.querySelectorAll('.faq-question');
 
-// faqQuestion.forEach(function(question){
-//     question.addEventListener('click', function(){
-//         let answer = question.parentElement.querySelector('.faq-answer');
-//         answer.classList.toggle('show');
-//     })
-// })
+questions.forEach((question)=>{
+    question.addEventListener('click', ()=> {
+        let answer = question.nextElementSibling;
+        let plusSign = question.querySelector('.plus-sign');
+        let minusSign = question.querySelector('.minus-sign');
+        let allAnswers = document.querySelectorAll('.faq-answer');
+        isExpanded = answer.classList.contains('expanded');
 
-faqQuestion.forEach((elem, index) => {
-    elem.addEventListener('click', () => {
-      answer.forEach((ans, ansIndex) => {
-        if (index === ansIndex) {
-          ans.style.display = ans.style.display === 'none' ? 'block' : 'none';
-        } else {
-          ans.style.display = 'none';
+        allAnswers.forEach((answer)=> {
+            answer.classList.remove('expanded');
+            answer.previousElementSibling.querySelector('.plus-sign').classList.add('show');
+            answer.previousElementSibling.querySelector('.minus-sign').classList.remove('active');
+        });
+
+        if (!isExpanded) {
+            answer.classList.add('expanded');
+            minusSign.classList.add('active');
+            plusSign.classList.add('none');
+            plusSign.classList.remove('show');
         }
-      });
-      // toggle plus and minus
-      plusSigns.forEach((plusSign) => {
-        plusSign.addEventListener('click', () => {
-          plusSign.style.display = 'none';
-          const minusSign = plusSign.nextElementSibling;
-          minusSign.style.display = 'inline';
-        });
-      });
-
-      minusSigns.forEach((minusSign) => {
-        minusSign.addEventListener('click', () => {
-          minusSign.style.display = 'none';
-          const plusSign = minusSign.previousElementSibling;
-          plusSign.style.display = 'inline';
-        });
-      });
     });
-  });
+});
+
+
+
 
   
 
-  
-
-// faqQuestion.forEach((elem)=>{
-//     elem.addEventListener('click', ()=>{
-//         minusSign.style.display = minusSign.style.display === 'none' ? 'block' : 'none';
-        
-//     })
-// }) 
-
-// menuOpen.onclick = function () {
-
-// }
